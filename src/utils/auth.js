d40fee69b154e4d352535d58134813c5a98bf002
Leonid Gladkikh <leogladkikh@yandex.ru>
2022-06-09 12:07:52 +0200

    bugfix

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,6 @@
-export const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3000';
+// export const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3000';
 // export const BASE_URL = 'https://api.leonid-movies.nomoredomains.work' || 'http://localhost:3000';
+export const BASE_URL = process.env.REACT_APP_BASE_URL = 'https://api.leonid-movies.nomoredomains.work npm run build';
 
 const HEADERS = {
   'Accept': 'application/json',
